8b63475cc8f5dc26ba8fbb2652911fac79a353d0
MegaINC2023 <esi-buceo@outlook.com>
2023-09-18 09:13:00 -0300

    version 1.1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("c://xampp/htdocs/Programacion-Web/view/head/header.php");
+?>
+
+
+
+
+
+
+<?php
+    require_once("c://xampp/htdocs/Programacion-Web/view/head/footer.php");
+?>
